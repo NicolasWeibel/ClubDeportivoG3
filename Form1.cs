3b340f1eb9ff0e75fe859b3571e899095ceb5c0a
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2024-10-11 03:20:38 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+using Org.BouncyCastle.Pqc.Crypto.Lms;
+using System.Data;
+
+namespace ClubDeportivoG3
+{
+    public partial class FormInicio : Form
+    {
+        public FormInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtInicio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "USUARIO")
+            {
+                txtUsuario.Text = "";
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "CONTRASEÑA")
+            {
+                txtPassword.Text = "";
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "CONTRASEÑA";
+                txtPassword.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            DataTable tablaLogin = new DataTable(); // es la que recibe los datos desde el formulario
+            Datos.Usuarios dato = new Datos.Usuarios(); // variable que contiene todas las caracteristicas de la clase
+            tablaLogin = dato.Log_Usu(txtUsuario.Text, txtPassword.Text);
+            if (tablaLogin.Rows.Count > 0)
+            {
+                // quiere decir que el resultado tiene 1 fila por lo que el  usuario EXISTE
+            MessageBox.Show("Ingreso exitoso");
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o password incorrecto");
+            }
+
+        }
+    }
+}
